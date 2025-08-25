bd9c56ab6ed30e51e25914775e12e95785ad5962
e1q23079 <e1q23079@outlook.jp>
2025-08-25 17:55:12 +0900

    履歴管理機能実装

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,16 +1,28 @@
+// 履歴管理
+const history = []
+
 export default (io, socket) => {
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (data) => {
+    history.push(data) // 履歴に追加
     socket.broadcast.emit("enterEvent", data)
   })
 
   // 退室メッセージをクライアントに送信する
   socket.on("exitEvent", (data) => {
+    history.push(data) // 履歴に追加
     socket.broadcast.emit("exitEvent", data)
   })
 
   // 投稿メッセージを送信する
   socket.on("publishEvent", (data) => {
+    history.push(data) // 履歴に追加
     io.sockets.emit("publishEvent", data)
+  })
+
+  // 投稿履歴をクライアントに送信する
+  socket.on("historyEvent", () => {
+    console.log("history", history)
+    socket.emit("historyEvent", history)
   })
 }
