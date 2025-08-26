fa2f48cfe7fad5616b7d5dd1d888a8f3d13f6c6f
konomi50 <daikon.okonomi@gmail.com>
2025-08-26 11:43:10 +0900

    markedの改行オプション設定

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -3,6 +3,9 @@ import { inject, ref, reactive, onMounted, useTemplateRef, computed} from "vue" 
 import socketManager from '../socketManager.js'
 import { marked } from "marked"
 
+// markedの改行オプションをtrueに設定
+marked.setOptions({breaks : true});
+
 // #region global state
 const userName = inject("userName")
 // #endregion
