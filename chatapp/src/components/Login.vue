62e3c11b2083f969b44f3b0d8785a4203e75552e
HitsujiRere <hitsujirere@gmail.com>
2025-08-26 12:46:59 +0900

    formatterの設定

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -1,41 +1,41 @@
 <script setup>
-  import { inject, ref } from "vue"
-  import { useRouter } from "vue-router"
-  import socketManager from '../socketManager.js'
+import { inject, ref } from "vue"
+import { useRouter } from "vue-router"
+import socketManager from '../socketManager.js'
 
-  // #region global state
-  const userName = inject("userName")
-  // #endregion
+// #region global state
+const userName = inject("userName")
+// #endregion
 
-  // #region local variable
-  const router = useRouter()
-  const socket = socketManager.getInstance()
-  // #endregion
+// #region local variable
+const router = useRouter()
+const socket = socketManager.getInstance()
+// #endregion
 
-  // #region reactive variable
-  const inputUserName = ref("")
-  // #endregion
+// #region reactive variable
+const inputUserName = ref("")
+// #endregion
 
-  // #region browser event handler
-  // 入室メッセージをクライアントに送信する
-  const onEnter = () => {
-    // ユーザー名が入力されているかチェック
-    if (inputUserName.value === "") {
-      alert("ユーザー名を入力してください")
-      return
-    }
-    // 入室メッセージを送信
-    socket.emit("enterEvent", {
-      type: "enter",
-      name: inputUserName.value,
-      datetime: Date.now()
-    })
-    // 全体で使用するnameに入力されたユーザー名を格納
-    userName.value = inputUserName.value
-    // チャット画面へ遷移
-    router.push({ name: "chat" })
+// #region browser event handler
+// 入室メッセージをクライアントに送信する
+const onEnter = () => {
+  // ユーザー名が入力されているかチェック
+  if (inputUserName.value === "") {
+    alert("ユーザー名を入力してください")
+    return
   }
-  // #endregion
+  // 入室メッセージを送信
+  socket.emit("enterEvent", {
+    type: "enter",
+    name: inputUserName.value,
+    datetime: Date.now()
+  })
+  // 全体で使用するnameに入力されたユーザー名を格納
+  userName.value = inputUserName.value
+  // チャット画面へ遷移
+  router.push({ name: "chat" })
+}
+// #endregion
 </script>
 
 <template>
@@ -50,9 +50,9 @@
 </template>
 
 <style scoped>
-  .user-name-text {
-    width: 200px;
-    border: 1px solid #888;
-    margin-bottom: 16px;
-  }
+.user-name-text {
+  width: 200px;
+  border: 1px solid #888;
+  margin-bottom: 16px;
+}
 </style>
