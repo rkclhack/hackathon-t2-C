510ef835bbd7cb286d4f7410c77dc941c79a1517
e1q23079 <e1q23079@outlook.jp>
2025-08-26 11:41:11 +0900

    ポップアップしていないとき非表示

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -109,10 +109,13 @@
   // #endregion
 
   const pipRef = useTemplateRef("pipRef")
+  // Picture-in-Picture 状態
+  const pipStatus = ref(false)
   const openPip = async () => {
     const pipWindow = await window.documentPictureInPicture.requestWindow({
     });
     pipWindow.document.body.append(pipRef.value);
+    pipStatus.value = true;
   }
 </script>
 
@@ -154,7 +157,7 @@
   <button class="button-normal" @click="openPip">Picture-in-Picture Open</button>
 
   <!-- Picture-in-Picture -->
-  <div ref="pipRef" class="mx-auto my-5 px-4">
+  <div ref="pipRef" class="mx-auto my-5 px-4" v-show="pipStatus">
     <h1 class="text-h3 font-weight-medium">Vue.js Chat チャットルーム</h1>
     <div class="mt-10">
       <p>ログインユーザ：{{ userName }}さん</p>
